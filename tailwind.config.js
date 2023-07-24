ad7acfbab86d441ed6c590f5ba2fd25ed3c6bc94
Sravan SP <sravansp92@gmail.com>
2023-07-24 17:33:11 +0530

    bug fix logo slider isue

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,16 @@ module.exports = {
         'xs': '500px',
         // // => @media (min-width: 500px) { ... }
         // 'tab': {'min': '768px', 'max': '1023px'},
-      // => @media (min-width: 768px and max-width: 1023px and orientation : portrait) { ... }
+        // => @media (min-width: 768px and max-width: 1023px and orientation : portrait) { ... }
+      },
+      animation: {
+        scroll: 'scroll 40s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(-250px * 9))' },
+        },
       },
     },
 
